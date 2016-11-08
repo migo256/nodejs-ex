df75ad74d8686d9f693a37c0dafbc219065a22a6
mgollan <mgollan@OMP-8NL7322.omp-dom.pb.omp.de>
2016-11-08 15:53:08 +0100

    revert change: from === to == in equal null check

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
-if (mongoURL === null && process.env.DATABASE_SERVICE_NAME) {
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
       mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
       mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
@@ -51,10 +51,10 @@ var db = null,
     dbDetails = new Object();
 
 var initDb = function(callback) {
-  if (mongoURL === null) return;
+  if (mongoURL == null) return;
 
   var mongodb = require('mongodb');
-  if (mongodb === null) return;
+  if (mongodb == null) return;
 
   mongodb.connect(mongoURL, function(err, conn) {
     if (err) {
