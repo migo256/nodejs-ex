d50bdffdb86622530fd9102e578c73a9a5333a80
mgollan <mgollan@OMP-8NL7322.omp-dom.pb.omp.de>
2016-11-08 15:45:33 +0100

    fixed debug output

--- server.js
+++ server.js
@@ -35,17 +35,17 @@ if (mongoURL === null && process.env.DATABASE_SERVICE_NAME) {
   }
 }
 
-console.log('port: &s', port);
-console.log('ip: &s', ip);
-console.log('mongoServiceName: &s', mongoServiceName);
-console.log('mongoHost: &s', mongoHost);
-console.log('mongoPort: &s', mongoPort);
-console.log('mongoDatabase: &s', mongoDatabase);
-console.log('mongoPassword: &s', mongoPassword);
-console.log('mongoUser: &s', mongoUser);
+console.log('port: %s', port);
+console.log('ip: %s', ip);
+console.log('mongoServiceName: %s', mongoServiceName);
+console.log('mongoHost: %s', mongoHost);
+console.log('mongoPort: %s', mongoPort);
+console.log('mongoDatabase: %s', mongoDatabase);
+console.log('mongoPassword: %s', mongoPassword);
+console.log('mongoUser: %s', mongoUser);
 
-console.log('mongoURL: &s', mongoURL);
-console.log('mongoURLLabel: &s', mongoURLLabel);
+console.log('mongoURL: %s', mongoURL);
+console.log('mongoURLLabel: %s', mongoURLLabel);
 
 var db = null,
     dbDetails = new Object();
